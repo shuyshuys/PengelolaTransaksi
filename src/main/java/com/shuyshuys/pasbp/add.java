b6843c360360ad1c01f4fa755205f9f436451d11
shuyshuys <ilvrynsetsuna@protonmail.com>
2022-06-02 12:28:37 +0700

    add additional method

---
+++ src/main/java/com/shuyshuys/pasbp/add.java
@@ -0,0 +1,31 @@
+package com.shuyshuys.pasbp;
+
+public class add {
+    public static void border() {
+        System.out.println("=================================================");
+    }
+
+    public static void salahInput() {
+        System.out.println("Pilihan salah, mohon ulangi!");
+    }
+
+    public static void cls() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void sleep5s() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void exit() {
+        border();
+        System.out.println("Terima Kasih Telah Menggunakan Program ini!");
+        border();
+        System.exit(0);
+    }
+}
