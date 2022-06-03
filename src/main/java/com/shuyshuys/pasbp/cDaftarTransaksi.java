bd991502a1175fd0491c22890c646831ff01a772
shuyshuys <ilvrynsetsuna@protonmail.com>
2022-06-03 21:34:40 +0700

    hotfix for delete transaction

--- src/main/java/com/shuyshuys/pasbp/cDaftarTransaksi.java
+++ src/main/java/com/shuyshuys/pasbp/cDaftarTransaksi.java
@@ -9,7 +9,7 @@ public class cDaftarTransaksi {
         jumlah = 0;
     }
 
-    public void tambahTransaksi(cTransaksi trBaru) {
+    public void enqueueTransaksi(cTransaksi trBaru) {
         if (tail == null) {
             head = tail = trBaru;
         } else {
@@ -20,18 +20,57 @@ public class cDaftarTransaksi {
         System.out.println("Transaksi berhasil ditambahkan");
     }
 
-    public void lihatTransaksi() {
-        int i = 1;
-        System.out.println("Daftar Transaksi");
-        System.out.println("NO  KODE NAMA\tBARANG\t\tJUMLAH\tSTATUS");
-        for (cTransaksi t = head; t != null; t = t.next) {
-            System.out.println(i + ".  " + t.getKode() + "  " + t.getNamaPembeli() + "\t[" + t.getBarang().getNama()
-                    + "]\t[" + t.getJumlahBarang() + "]\t[" + t.getStatus() + "]");
-            i++;
+    public void peekTransaksi() {
+        if (head == null && tail == null) {
+            System.out.println("Transaksi kosong!");
+            add.border();
+        } else {
+            int i = 1;
+            System.out.println("Daftar Transaksi");
+            System.out.println("NO  KODE NAMA\t\tBARANG\t\tJUMLAH\tSTATUS");
+            for (cTransaksi t = head; t != null; t = t.next) {
+                if (t.getNamaPembeli().length() >= 6) {
+                    if (t.getBarang().getNama().length() >= 6) {
+                        System.out.println(i + ".  " +
+                                t.getKode() + "  " +
+                                t.getNamaPembeli() + "\t[" +
+                                t.getBarang().getNama() + "]\t[" +
+                                t.getJumlahBarang() + "]\t[" +
+                                t.getStatus() + "]");
+                        i++;
+                    } else {
+                        System.out.println(i + ".  " +
+                                t.getKode() + "  " +
+                                t.getNamaPembeli() + "\t[" +
+                                t.getBarang().getNama() + "]\t\t[" +
+                                t.getJumlahBarang() + "]\t[" +
+                                t.getStatus() + "]");
+                        i++;
+                    }
+                } else {
+                    if (t.getBarang().getNama().length() >= 6) {
+                        System.out.println(i + ".  " +
+                                t.getKode() + "  " +
+                                t.getNamaPembeli() + "\t\t[" +
+                                t.getBarang().getNama() + "]\t[" +
+                                t.getJumlahBarang() + "]\t[" +
+                                t.getStatus() + "]");
+                        i++;
+                    } else {
+                        System.out.println(i + ".  " +
+                                t.getKode() + "  " +
+                                t.getNamaPembeli() + "\t\t[" +
+                                t.getBarang().getNama() + "]\t\t[" +
+                                t.getJumlahBarang() + "]\t[" +
+                                t.getStatus() + "]");
+                        i++;
+                    }
+                }
+            }
         }
     }
 
-    public void deQueue() { // admin atau pembeli
+    public void deQueueTransaksi() { // admin atau pemilik
         cTransaksi temp;
         if (head == null) {
             head = null;
@@ -63,30 +102,57 @@ public class cDaftarTransaksi {
         return null;
     }
 
-    public void hapusTransaksi(String namaPembeli, String barang) {
+    public void cariHapusTransaksi(String namaPembeli, String barang) {
         cTransaksi temp = head;
+
+        System.out.println("  -temp.getNamaPembeli() = " + temp.getNamaPembeli());
+        System.out.println("  -temp.getBarang().getNama() = " + temp.getBarang().getNama());
+        System.out.println("  -tail = " + tail.getNamaPembeli());
+        System.out.println("  -tail = " + tail.getBarang().getNama());
+
         if (temp.getNamaPembeli().equalsIgnoreCase(namaPembeli)
                 && temp.getBarang().getNama().equalsIgnoreCase(barang)) {
-            System.out.println("temp.getNamaPembeli() = " + temp.getNamaPembeli());
-            System.out.println("temp.getBarang().getNama() = " + temp.getBarang().getNama());
-            head = head.next;
-            temp.next = null;
+            if (temp.next == null) {
+                temp = tail = null;
+                System.out.println("temp = tail = null");
+            } else {
+                // temp =
+                temp = temp.next;
+                System.out.println("temp = temp.next");
+                // temp.next = null;
+            }
             jumlah--;
             System.out.println("Transaksi berhasil dihapus");
         } else {
             while (temp.next != null) {
                 if (temp.next.getNamaPembeli().equalsIgnoreCase(namaPembeli)
                         && temp.next.getBarang().getNama().equalsIgnoreCase(barang)) {
-                    System.out.println("temp.next.getNamaPembeli() = " + temp.next.getNamaPembeli());
-                    System.out.println("temp.next.getBarang().getNama() = " + temp.next.getBarang().getNama());
+                    // detele this if done
+                    System.out.println("  -temp.next = " + temp.next.getNamaPembeli());
+                    System.out.println("  -temp.next = " + temp.next.getBarang().getNama());
+                    System.out.println("  -tail = " + tail.getNamaPembeli());
+                    System.out.println("  -tail = " + tail.getBarang().getNama());
+                    // System.out.println("temp.next.next = " +
+                    // temp.next.next.getBarang().getNama());
+
+                    // until here
 
                     if (temp.next.next != null) {
+                        System.out.println("  -temp.next.next = " + temp.next.next.getBarang().getNama());
+                        // temp.next = temp.next.next;
+                        // temp.next.next = null;
                         temp.next = temp.next.next;
-                        temp.next.next = null;
+                        tail = temp.next;
+                        // temp.next.next = null;
                         System.out.println("if terhapus");
                     } else {
                         // temp = temp.next;
+                        // temp.next = null;
+                        tail = temp;
+                        System.out.println("  -temp.next = " + temp.next.getBarang().getNama());
                         temp.next = null;
+
+                        // tail = null;
                         System.out.println("else terhapus");
                     }
                     jumlah--;
@@ -94,7 +160,7 @@ public class cDaftarTransaksi {
                     break;
                 }
                 temp = temp.next;
-                System.out.println("tidak ditemukan, try next");
+                System.out.println("tidak ditemukan, try next simpul");
             }
         }
     }
