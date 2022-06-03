476dfc32a7870f2dd6eb5d4f5679b701d824fcfb
shuyshuys <ilvrynsetsuna@protonmail.com>
2022-06-03 15:00:26 +0700

    update hapusTr,add tab for lihatTr

--- src/main/java/com/shuyshuys/pasbp/cDaftarTransaksi.java
+++ src/main/java/com/shuyshuys/pasbp/cDaftarTransaksi.java
@@ -9,12 +9,12 @@ public class cDaftarTransaksi {
         jumlah = 0;
     }
 
-    public void tambahTransaksi(cTransaksi baru) {
+    public void tambahTransaksi(cTransaksi trBaru) {
         if (tail == null) {
-            head = tail = baru;
+            head = tail = trBaru;
         } else {
-            tail.next = baru;
-            tail = baru;
+            tail.next = trBaru;
+            tail = trBaru;
         }
         jumlah++;
         System.out.println("Transaksi berhasil ditambahkan");
@@ -23,9 +23,10 @@ public class cDaftarTransaksi {
     public void lihatTransaksi() {
         int i = 1;
         System.out.println("Daftar Transaksi");
+        System.out.println("NO  KODE NAMA\tBARANG\t\tJUMLAH\tSTATUS");
         for (cTransaksi t = head; t != null; t = t.next) {
-            System.out.println(i + ". " + t.getKode() + " " + t.getNamaPembeli() + " " + t.getBarang().getNama() + " "
-                    + t.getJumlahBarang() + " " + t.getStatus());
+            System.out.println(i + ".  " + t.getKode() + "  " + t.getNamaPembeli() + "\t[" + t.getBarang().getNama()
+                    + "]\t[" + t.getJumlahBarang() + "]\t[" + t.getStatus() + "]");
             i++;
         }
     }
@@ -66,21 +67,34 @@ public class cDaftarTransaksi {
         cTransaksi temp = head;
         if (temp.getNamaPembeli().equalsIgnoreCase(namaPembeli)
                 && temp.getBarang().getNama().equalsIgnoreCase(barang)) {
+            System.out.println("temp.getNamaPembeli() = " + temp.getNamaPembeli());
+            System.out.println("temp.getBarang().getNama() = " + temp.getBarang().getNama());
             head = head.next;
             temp.next = null;
             jumlah--;
             System.out.println("Transaksi berhasil dihapus");
         } else {
             while (temp.next != null) {
-                if (temp.getNamaPembeli().equalsIgnoreCase(namaPembeli)
+                if (temp.next.getNamaPembeli().equalsIgnoreCase(namaPembeli)
                         && temp.next.getBarang().getNama().equalsIgnoreCase(barang)) {
-                    temp.next = temp.next.next;
-                    temp.next.next = null;
+                    System.out.println("temp.next.getNamaPembeli() = " + temp.next.getNamaPembeli());
+                    System.out.println("temp.next.getBarang().getNama() = " + temp.next.getBarang().getNama());
+
+                    if (temp.next.next != null) {
+                        temp.next = temp.next.next;
+                        temp.next.next = null;
+                        System.out.println("if terhapus");
+                    } else {
+                        // temp = temp.next;
+                        temp.next = null;
+                        System.out.println("else terhapus");
+                    }
                     jumlah--;
                     System.out.println("Transaksi berhasil dihapus");
                     break;
                 }
                 temp = temp.next;
+                System.out.println("tidak ditemukan, try next");
             }
         }
     }
