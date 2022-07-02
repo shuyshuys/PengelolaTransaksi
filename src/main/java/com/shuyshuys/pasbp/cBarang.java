161d3744d16b17a7e0a85056ee8cd23b9976e0aa
shuyshuys <ilvrynsetsuna@protonmail.com>
2022-07-02 21:18:59 +0700

    update cBarang to extends from cObject

--- src/main/java/com/shuyshuys/pasbp/cBarang.java
+++ src/main/java/com/shuyshuys/pasbp/cBarang.java
@@ -1,29 +1,10 @@
 package com.shuyshuys.pasbp;
 
-public class cBarang {
-    String nama;
-    int harga;
+public class cBarang extends cObject {
     cBarang next;
 
-    cBarang(String nama, int harga) {
-        this.nama = nama;
-        this.harga = harga;
-        System.out.println("Barang " + nama + " berhasil ditambahkan");
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public int getHarga() {
-        return harga;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public void setHarga(int harga) {
-        this.harga = harga;
+    cBarang(String namaBarang, int hargaBarang) {
+        super(namaBarang, hargaBarang);
+        this.laporanHarga = 0;
     }
 }
